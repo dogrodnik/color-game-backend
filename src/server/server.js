5a0de65771bc5b50b6a2585ec8c6a853cabef1bb
dogrodnik <d.ogrodnik96@gmail.com>
2020-01-22 15:27:01 +0100

    Add cors package

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const route = require("./routes/listRoute");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/", route);
 
