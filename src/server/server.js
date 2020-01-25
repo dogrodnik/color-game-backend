7ebf0d6c733201d8b6dfdc15a27f316a05ef7684
dogrodnik <d.ogrodnik96@gmail.com>
2020-01-25 16:50:52 +0100

    Set headers

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,17 @@ const route = require("./routes/listRoute");
 
 const app = express();
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://dogrodnik.github.io/color-game/"
+  ); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.use("/", route);
 
