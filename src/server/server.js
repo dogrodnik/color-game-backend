f71367511566a2bb36b227e83eb38559838aee44
dogrodnik <d.ogrodnik96@gmail.com>
2020-01-25 17:13:53 +0100

    Use cors

--- src/server/server.js
+++ src/server/server.js
@@ -4,17 +4,7 @@ const route = require("./routes/listRoute");
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://dogrodnik.github.io/color-game/"
-  ); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 app.use(express.json());
 app.use("/", route);
 
